5547df5464b35dd94b9332ea8c3e59caa303eb24
Ananya Singh <67984681+ananya-03@users.noreply.github.com>
2023-04-30 22:31:16 +0800

    Update visualise.py

--- visualise.py
+++ visualise.py
@@ -1,74 +1,32 @@
-import numpy as np
-import os
-from brax import envs
-from brax.io import html
-import streamlit.components.v1 as components
 import streamlit as st
-from diffmimic.utils.io import deserialize_qp, serialize_qp
-from diffmimic.mimic_envs import register_mimic_env
+import pythreejs as p3
+import numpy as np
 
-register_mimic_env()
+# Define a function to load the 3D model from a .npy file
+def load_model(file):
+    # Load the .npy file using numpy.load
+    data = np.load(file)
+    # Extract the vertices and faces from the data array
+    vertices = data["vertices"]
+    faces = data["faces"]
+    return vertices, faces
 
-st.title('DiffMimic - Visualization')
-with st.expander("Readme"):
-    st.markdown(
-        'Input modes:'
-        '\n- File Path: input the local file path to the .npy trajectory file.'
-        '\n- Experiment Log: select trajectories by browsing experiment logs.'
-        '\n- Direct Upload: directly upload the .npy trajectory file.')
+# Create a file uploader using Streamlit's built-in function
+uploaded_file = st.file_uploader("Upload a .npy file")
 
+if uploaded_file is not None:
+    # Call the load_model function to load the 3D model from the uploaded file
+    vertices, faces = load_model(uploaded_file)
 
-def show_rollout_traj(rollout_traj, tag):
-    if len(rollout_traj.shape) == 3:
-        seed = st.slider(f'Random seed ({tag})', 0, rollout_traj.shape[1] - 1, 0)
-        rollout_traj = rollout_traj[:, seed]
-    if rollout_traj.shape[-1] == 182:
-        system_cfg = 'humanoid'
-    elif rollout_traj.shape[-1] == 247:
-        system_cfg = 'smpl'
-    else:
-        system_cfg = 'swordshield'
-
-    rollout_qp = [deserialize_qp(rollout_traj[i]) for i in range(rollout_traj.shape[0])]
-    rollout_traj = serialize_qp(deserialize_qp(rollout_traj))
-
-    env = envs.get_environment(
-        env_name="humanoid_mimic",
-        system_config=system_cfg,
-        reference_traj=rollout_traj,
+    # Use Pythreejs to create a 3D mesh object from the vertices and faces
+    geometry = p3.BufferGeometry(
+        attributes={
+            "position": p3.BufferAttribute(vertices),
+        },
+        index=p3.BufferAttribute(faces.ravel()),
     )
-    components.html(html.render(env.sys, rollout_qp), height=500)
+    material = p3.MeshStandardMaterial(color="lightgray", metalness=0.5, roughness=0.5)
+    mesh = p3.Mesh(geometry=geometry, material=material)
 
-
-def main():
-    tab_file_path, tab_experiment_log, tab_direct_upload = st.tabs(["File Path", "Experiment Log", "Direct Upload"])
-
-    with tab_file_path:
-        file_path = st.text_input('Path to trajectory')
-        if os.path.isfile(file_path):
-            with open(file_path, 'rb') as f:
-                rollout_traj = np.load(f)
-                show_rollout_traj(rollout_traj, 'FP')
-        else:
-            st.warning('Please input a valid path', icon="⚠️")
-
-    with tab_experiment_log:
-        try:
-            exp_name = st.selectbox('Experiment name', tuple(os.listdir('logs')))
-            all_files = sorted([x for x in os.listdir(os.path.join('logs', exp_name)) if x.endswith(".npy")])
-            file_name = st.selectbox('File name', tuple(all_files), index=len(all_files)-1)
-            with open(os.path.join('logs', exp_name, file_name), 'rb') as f:
-                rollout_traj = np.load(f)
-                show_rollout_traj(rollout_traj, 'EL')
-        except:
-            st.warning('Please try other input modes', icon="⚠️")
-
-    with tab_direct_upload:
-        uploaded_file = st.file_uploader("Upload a trajectory")
-        if uploaded_file is not None:
-            rollout_traj = np.load(uploaded_file)
-            show_rollout_traj(rollout_traj, 'DU')
-
-
-if __name__ == '__main__':
-    main()
+    # Use Streamlit to display the 3D mesh object
+    st.write(p3.Renderer(camera="perspective", scene=p3.Scene(children=[mesh]), width=400, height=400))

c5dcbe4ce500695b4887fb75f27f04600ad70547
Ananya Singh <67984681+ananya-03@users.noreply.github.com>
2023-05-02 00:50:15 +0800

    Update visualise.py

--- visualise.py
+++ visualise.py
@@ -3,28 +3,64 @@ from brax import envs
 from brax.io import html
 import streamlit.components.v1 as components
 import streamlit as st
+import glob
 
 from diffmimic.utils.io import deserialize_qp
 from diffmimic.mimic_envs import register_mimic_env
 
 register_mimic_env()
 
-# Ask for the path of the reference motion file
-ref_motion_path = st.text_input('Enter path to reference motion file:')
-try:
-    demo_traj = np.load(ref_motion_path)
-except FileNotFoundError:
-    st.warning('Reference motion file not found.')
 
-# Load reference motion file and extract the initial QP state and demo QP states
+traj_dir_list = [
+    'data/demo_aist',
+    'data/demo_amass',
+    '../ild_vis',
+]
+traj_dir = st.selectbox('Motion directory', traj_dir_list)
+
+fname_dict = {fname.split('/')[-1]: fname for fname in glob.glob("{}/*.npy".format(traj_dir))}
+ref_motion = st.selectbox('Reference motion', fname_dict.keys())
+
+demo_traj = np.load(fname_dict[ref_motion])
+
 if len(demo_traj.shape) == 3:
     demo_traj = demo_traj[:, 1]  # vis env 0
+
 init_qp = deserialize_qp(demo_traj[0])
 demo_qp = [deserialize_qp(demo_traj[i]) for i in range(demo_traj.shape[0])]
 
-# Create environment and render HTML visualization
 env = envs.create(env_name='humanoid_mimic',
                   system_config='smpl',
                   reference_traj=demo_traj,
                   )
 components.html(html.render(env.sys, demo_qp), height=500)
+
+
+
+traj_dir_list = [
+    'data/demo_aist',
+    'data/demo_amass',
+    '../ild_vis',
+]
+traj_dir = st.selectbox('Reference Motion directory', traj_dir_list)
+
+fname_dict = {fname.split('/')[-1]: fname for fname in glob.glob("{}/*.npy".format(traj_dir))}
+ref_motion = st.selectbox('Reference motion', fname_dict.keys())
+
+demo_traj = np.load(fname_dict[ref_motion])
+
+if len(demo_traj.shape) == 3:
+    demo_traj = demo_traj[:, 1]  # vis env 0
+
+init_qp = deserialize_qp(demo_traj[0])
+demo_qp = [deserialize_qp(demo_traj[i]) for i in range(demo_traj.shape[0])]
+
+env = envs.create(env_name='humanoid_mimic',
+                  system_config='smpl',
+                  reference_traj=demo_traj,
+                  )
+components.html(html.render(env.sys, demo_qp), height=500)
+
+
+
+edit this script such that it has a text input box 
